60278bb076a75c6a4dabe6384e8871e4021fc231
Samuele <bazlite2@yahoo.it>
2024-08-10 20:37:57 +0200

    add deployment

---
+++ scripts/ftp_sync.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Michael Hutter, 20.11.2021
+# This Script can be used to synchronize a remote FTP directory and a local directory
+HOST='ftp.bestmessina.org'
+USER=$FTP_USER
+PASS=$FTP_PASSWORD
+SERVERFOLDER='/htdocs'
+PCFOLDER='./build'
+CopyMoreThanOneFileSimultaneously="--parallel=10"
+
+CopyServerdataToLocal=0 # 0=Upload, 1=Download
+IgnoreSubdirectories=0
+ContinuePartiallyTransmittedFiles=0
+DontOverwriteNewerExistingFiles=0 # If this is used ContinuePartiallyTransmittedFiles will not work!
+
+DeleteAdditionalFilesInDestinationDir=1 # Deletes files in DestDir which are not present in SourceDir
+RemoveSourceFilesAfterTransfer=0 # Moves the files instead of copying them
+
+ExcludeParams='--exclude-glob .* --exclude-glob .*/' # Exclude (hidden) files and direcories -> starting with a dot
+
+OnlyShowChangesButDontChangeFiles=0 # DryRun mode
+OutputAsMuchInfoAsPossible=1 # Verbose mode
+
+################################################################################
+
+if [ $CopyServerdataToLocal -eq 1 ]; then
+   if [ $OutputAsMuchInfoAsPossible -eq 1 ]; then
+      echo "Modus=Download"
+   fi
+   DIRECTORIES="$SERVERFOLDER $PCFOLDER"
+else
+   if [ $OutputAsMuchInfoAsPossible -eq 1 ]; then
+      echo "Modus=Upload"
+   fi
+   REVERSE='--reverse'
+   DIRECTORIES="$PCFOLDER $SERVERFOLDER"
+fi
+if [ $IgnoreSubdirectories -eq 1 ]; then
+   IGNORESUBDIRS='--no-recursion'
+fi
+
+if [ $ContinuePartiallyTransmittedFiles -eq 1 ]; then
+   CONTINUEFILES='--continue'
+fi
+if [ $DontOverwriteNewerExistingFiles -eq 1 ]; then
+   ONLYNEWER='--only-newer'
+fi
+if [ $DeleteAdditionalFilesInDestinationDir -eq 1 ]; then
+   DELETE='--delete'
+fi
+if [ $RemoveSourceFilesAfterTransfer -eq 1 ]; then
+   REMOVE='--Remove-source-files'
+fi
+if [ $OnlyShowChangesButDontChangeFiles -eq 1 ]; then
+   DRYRUN='--dry-run'
+fi
+if [ $OutputAsMuchInfoAsPossible -eq 1 ]; then
+   VERBOSE='--verbose'
+fi
+
+lftp -f "
+open $HOST
+user $USER $PASS
+set ftp:ssl-allow no
+lcd $PCFOLDER
+mirror $DRYRUN $REVERSE $IGNORESUBDIRS $DELETE $REMOVE $CONTINUEFILES $ONLYNEWER $CopyMoreThanOneFileSimultaneously --use-cache $ExcludeParams $VERBOSE $DIRECTORIES
+bye
+"
