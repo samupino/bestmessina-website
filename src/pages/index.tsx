c3659c5f345f3fee3ff7f505475b71213e74fd62
Samuele Pino <samp@bendingspoons.com>
2024-08-16 14:36:14 +0200

    center image on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,7 @@ import styles from "@site/src/css/index.module.css";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header
-      className={clsx("hero hero--primary", styles.heroBanner)}
-      style={{
-        backgroundImage: `url('/img/sunset_hug.jpg')`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
